738574ff6596c8e41672c3ddb624697d51101514
nksmkj7 <nksmkj7@gmail.com>
2022-12-24 21:51:16 +0545

    fix: translation fix

--- src/common/pipes/i18n-exception-filter.pipe.ts
+++ src/common/pipes/i18n-exception-filter.pipe.ts
@@ -114,7 +114,8 @@ export class I18nExceptionFilterPipe implements ExceptionFilter {
         'max',
         'isPhoneNumber',
         'isValidForeign',
-        'isNumberString'
+        'isNumberString',
+        'unique'
       ];
       const item = errors[i];
       let message = [];
@@ -131,6 +132,7 @@ export class I18nExceptionFilterPipe implements ExceptionFilter {
               validationKey = title;
               validationArgument = argument;
             }
+            
             const message = await this.i18n.translate(
               `validation.${validationKey}`,
               {
@@ -194,7 +196,6 @@ export class I18nExceptionFilterPipe implements ExceptionFilter {
     }
     // console.log(validationData, 'validation data is --->');
     // for await (const item of validationData) {
-    //   console.log('from inside --->');
     //   // validationData.forEach((item) => {
     //   if (item?.errors && item.errors.length) {
     //     const i18nFilePath = path.join(__dirname, '../../../src/i18n/');
